f1a70d859ad9be3ec989eda8867fdaef28ced74d
909054rutu <rutujashelke370@gmail.com>
2025-12-17 17:03:09 +0530

    updated the route for otp verification - GT.

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,15 @@ use Illuminate\Support\Facades\Hash;
 use App\Http\Controllers\HawkerController;
 use App\Http\Controllers\VendorAuthController;
 
+<<<<<<< HEAD
 
 Route::post('/register_hawker', [HawkerController::class, 'registerHawker']);
 
 
 Route::post('/vendor/register', [VendorAuthController::class, 'register']);
 Route::post('/vendor/verify-otp', [VendorAuthController::class, 'verifyOtp']);
+=======
+Route::post('/register_hawker', [HawkerController::class, 'registerHawker']);
+
+Route::post('/verify_hawker', [OTPController::class, 'validateOTP']);
+>>>>>>> 865a02b (updated the route for otp verification - GT.)
